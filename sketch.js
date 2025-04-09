818d968dadc3f19223d852fd25c927f42917fb29
fatma-hashimi <fya222@nyu.edu>
2025-04-09 17:39:02 -0400

    base functioning buttons

--- sketch.js
+++ sketch.js
@@ -1,46 +1,86 @@
 // ----------------------------------- Button Variables
 let titleBeginButton;
 let startTimerButton;
-let increaseTimeButton;
-let decreaseTimeButton;
+let increaseTimerButton;
+let decreaseTimerButton;
 let cancelTimerButton;
+let menuButton;
 
 // ----------------------------------- Pages
 let titlePage = true;
 let homePage = false;
 let menuPage = false;
-let foodSelectionPage = false;
 
 // ----------------------------------- Setup
+
 function setup() {
   createCanvas(450, 450);
+  imageMode(CENTER);
+
+  createButtons();
 }
 
 // ----------------------------------- Draw
+
 function draw() {
   background(220);
-  pageChanger()
+  pageChanger();
 }
 
 // ----------------------------------- Button Functions
-function titleButton() {
-  button = createImg('Media/cereal.png', 'Cereal');
-  button.position(90, 90);
-  button.size(250, 250);
-  button.mousePressed();
+
+function createButtons() {
+  titleBeginButton = createImg('Media/cereal.png', 'Cereal');
+  titleBeginButton.position(100, 100);
+  titleBeginButton.size(250, 250);
+  titleBeginButton.mouseClicked(() => {
+    console.log("entered home page");
+    titlePage = false;
+    homePage = true;
+  });
+
+  // need to change start timer button location later - its in the position of the selected food right now
+  startTimerButton = createImg('Media/toast.png', 'Toast');
+  startTimerButton.position(100, 70);
+  startTimerButton.size(250, 250);
+  startTimerButton.mouseClicked(() => {
+    console.log("timer started");
+  });
+
+  decreaseTimerButton = createImg('Media/cereal.png', 'Toast');
+  decreaseTimerButton.position(30, 140);
+  decreaseTimerButton.size(130, 130);
+  decreaseTimerButton.mouseClicked(() => {
+    console.log("-5 minutes");
+  });
+
+  increaseTimerButton = createImg('Media/cereal.png', 'Toast');
+  increaseTimerButton.position(290, 140);
+  increaseTimerButton.size(130, 130);
+  increaseTimerButton.mouseClicked(() => {
+    console.log("+5 minutes");
+  });
+
+  startTimerButton.hide();
+  increaseTimerButton.hide();
+  decreaseTimerButton.hide();
+  // cancelTimerButton.hide();
+  // menuButton.hide();
 }
 
 // ----------------------------------- Page Changer
 
 function pageChanger() {
   if (titlePage) {
-    // button
-    titleButton();
-    // drawing
+    titleBeginButton.show();
+    startTimerButton.hide();
   }
 
   if (homePage) {
-    // buttons
-
+    titleBeginButton.hide();
+    startTimerButton.show();
+    increaseTimerButton.show();
+    decreaseTimerButton.show();
   }
+
 }
